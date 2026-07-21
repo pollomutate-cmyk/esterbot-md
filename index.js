5e7cdcfce40de5a80d4f40f55007d9fb180e3f60
pollomutate-cmyk <pollomutate@gmail.com>
2026-07-21 21:00:15 +0000

    fix puerto

--- index.js
+++ index.js
@@ -258,3 +258,8 @@ setInterval(() => {
 }, 1000 * 60 * 30);
 
 iniciarBot();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => { res.send('Bot Activo'); });
+app.listen(port, () => { console.log('Puerto abierto'); });
